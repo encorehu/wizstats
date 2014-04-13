b545fc279094bf0f3f7d51c69ee2f4af391e1065
encorehu <huyoo353@126.com>
2014-04-13 22:06:41 +0800

    change back 80->675

--- updateshares.php
+++ updateshares.php
@@ -23,12 +23,12 @@ if( isLocked() ) die( "Already running.\n" );
 $link = pg_Connect("dbname=$psqldb user=$psqluser password='$psqlpass' host=$psqlhost", PGSQL_CONNECT_FORCE_NEW );
 
 
-$sql = "select to_timestamp((date_part('epoch', time)::integer / 80::integer) * 80::integer) at time zone 'UTC' as lst from public.shares where server=$serverid order by id desc limit 1";
+$sql = "select to_timestamp((date_part('epoch', time)::integer / 675::integer) * 675::integer) at time zone 'UTC' as lst from public.shares where server=$serverid order by id desc limit 1";
 $result = pg_exec($link, $sql);
 $row = pg_fetch_array($result, 0);
 $latestsharetime = $row["lst"];
 
-$sql = "select to_timestamp(((date_part('epoch', (select time from wizkid057.stats_shareagg order by time desc limit 1))::integer / 80::integer) * 80::integer)+80::integer) at time zone 'UTC' as fst";
+$sql = "select to_timestamp(((date_part('epoch', (select time from wizkid057.stats_shareagg order by time desc limit 1))::integer / 675::integer) * 675::integer)+675::integer) at time zone 'UTC' as fst";
 $result = pg_exec($link, $sql);
 
 if(pg_num_rows($result) > 0){
@@ -48,16 +48,16 @@ $sql = "insert into public.users(username) select distinct username from public.
 $result = pg_exec($link, $sql);
 
 $sql = "INSERT INTO $psqlschema.stats_shareagg (server, time, user_id, accepted_shares, rejected_shares, blocks_found, hashrate)
-select server, to_timestamp((date_part('epoch', time)::integer / 80::integer) * 80::integer) AS ttime, user_id,
+select server, to_timestamp((date_part('epoch', time)::integer / 675::integer) * 675::integer) AS ttime, user_id,
 0+SUM(((our_result::integer) * pow(2,(targetmask-32)))) as acceptedshares, COUNT(*)-SUM(our_result::integer) as rejectedshares, SUM(upstream_result::integer) as blocksfound,
-((SUM(((our_result::integer) * pow(2,(targetmask-32)))) * 4294967296) / 80) AS hashrate
-from public.shares where time > '$firstsharetime' and to_timestamp((date_part('epoch', time)::integer / 80::integer) * 80::integer) < '$latestsharetime' and server=$serverid group by ttime, server, user_id;";
+((SUM(((our_result::integer) * pow(2,(targetmask-32)))) * 4294967296) / 675) AS hashrate
+from public.shares where time > '$firstsharetime' and to_timestamp((date_part('epoch', time)::integer / 675::integer) * 675::integer) < '$latestsharetime' and server=$serverid group by ttime, server, user_id;";
 
 $sql = "INSERT INTO $psqlschema.stats_shareagg (server, time, user_id, accepted_shares, rejected_shares, blocks_found, hashrate)
-select server, to_timestamp((date_part('epoch', time)::integer / 80::integer) * 80::integer) at time zone 'UTC' AS ttime, users.id as user_id,
+select server, to_timestamp((date_part('epoch', time)::integer / 675::integer) * 675::integer) at time zone 'UTC' AS ttime, users.id as user_id,
 0+SUM(our_result::integer) as acceptedshares, COUNT(*)-SUM(our_result::integer) as rejectedshares, SUM(upstream_result::integer) as blocksfound,
-((SUM(our_result::integer) * POW(2, 40)) / 80) AS hashrate
-from public.shares left join users on shares.username=users.username where time > '$firstsharetime' and to_timestamp((date_part('epoch', time)::integer / 80::integer) * 80::integer) at time zone 'UTC'  < '$latestsharetime' and server=$serverid group by ttime, server, users.id;";
+((SUM(our_result::integer) * POW(2, 40)) / 675) AS hashrate
+from public.shares left join users on shares.username=users.username where time > '$firstsharetime' and to_timestamp((date_part('epoch', time)::integer / 675::integer) * 675::integer) at time zone 'UTC'  < '$latestsharetime' and server=$serverid group by ttime, server, users.id;";
 $result = pg_exec($link, $sql);
 
 unlink( LOCK_FILE );
